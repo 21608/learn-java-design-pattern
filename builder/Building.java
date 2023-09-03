b1c373e260173988f07d335ba3adea66602faf7b
21608 <oka00027@gmail.com>
2023-09-03 17:38:00 +0900

    Building(生成物)実装

--- builder/Building.java
+++ builder/Building.java
@@ -1,3 +1,22 @@
 public class Building {
-    
+    private Buns buns;
+    private Patty patty;
+    private Topping topping;
+
+    void setBuns(Buns buns) {
+        this.buns = buns;
+    }
+
+    void setPatty(Patty patty) {
+        this.patty = patty;
+    }
+
+    void setTopping(Topping topping) {
+        this.topping = topping;
+    }
+
+    @Override
+    public String toString(){
+        return "[Buns:" + this.buns + ", Patty:" + this.patty + ", Topping:" + this.topping + "]";
+    }
 }
