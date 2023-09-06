29b2654376975d4a20f1348e0d5690dceaafcee5
21608 <oka00027@gmail.com>
2023-09-06 18:14:30 +0900

    mainメソッド実装

--- builder/BuilderPattern.java
+++ builder/BuilderPattern.java
@@ -1,4 +1,10 @@
 public class BuilderPattern {
     public static void main(String[] args) {
+        Director d1 = new Director(new HamburgerBuilder());
+        Director d2 = new Director(new RiceburgerBuilder());
+        Building b1 = d1.construct();
+        Building b2 = d2.construct();
+        System.out.println(b1);
+        System.out.println(b2);
     }
 }
